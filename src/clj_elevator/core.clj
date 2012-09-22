6849c3cadaedb89811cb4e5d796ae1ffce504e6f
Theo Crevon <tcrevon@gmail.com>
2012-09-22 22:17:59 +0200

    First commit

---
+++ src/clj_elevator/core.clj
@@ -0,0 +1,1 @@
+(ns clj-elevator.core)
---
+++ test/clj_elevator/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-elevator.test.core
+  (:use [clj-elevator.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
