51c19238d30515aa0c9635b9f75157c0b7d0679a
Theo Crevon <tcrevon@gmail.com>
2012-09-23 09:14:29 -0700

    Update : valid dependencies for 0mq and jzmq

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject clj-elevator "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                         [clj-msgpack "0.2.0"]]
+                 [clj-msgpack "0.2.0"]
+                 [org.clojars.vaughan/jzmq "1.1.0"]
+                 [org.clojars.mikejs/clojure-zmq "2.0.7-SNAPSHOT"]]
+  :native-path "/usr/local/lib"
   :main clj-elevator.core)
