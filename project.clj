c9479ce0565229fc56480587f8b4a08b4b17bac3
Theo Crevon <tcrevon@gmail.com>
2012-09-23 15:54:46 +0200

    Update : base request/response messages handlers

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clj-elevator "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                         [clj-msgpack "0.2.0"]])
+                         [clj-msgpack "0.2.0"]]
+  :main clj-elevator.core)
